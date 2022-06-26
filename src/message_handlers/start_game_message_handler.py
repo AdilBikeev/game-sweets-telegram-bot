719a5eaffb85a18fc63bc2459eb09247d8f56833
AdilBikeev <bikeev1999@mail.ru>
2022-06-26 21:32:51 +0300

    Refactoring

--- src/message_handlers/start_game_message_handler.py
+++ src/message_handlers/start_game_message_handler.py
@@ -5,41 +5,48 @@ from telebot import TeleBot
 import private.storage as storage
 
 def init(bot: TeleBot):
-    MIN_SWEETS = 1
-    MAX_SWEETS = 100
+    # Значения для генерации изначального значения
+    MIN_SWEETS = 1 # минимальное кол-во 🍬
+    MAX_SWEETS = 100 # максимальное кол-во 🍬
 
     def process_digit_step(message):
         user_digit = message.text
         count_sweets = storage.get(message.chat.id)["count_sweets"]
 
-        if not user_digit.isdigit():
+        if (not user_digit.isdigit() or
+            int(user_digit) < 1 or
+            int(user_digit) > count_sweets):
                 msg = bot.reply_to(message, f'Ошибка ввода, пожалуйста введите число не более {count_sweets}')
                 bot.register_next_step_handler(msg, process_digit_step)
                 return
         
-        if int(user_digit) == count_sweets: # Если пользователь забрал оставшиеся конфеты
-            bot.send_message(message.chat.id, f'Ну все :( ты забрал оставшиеся конфеты. На столе ничего не осталось.')
+        if int(user_digit) == count_sweets: # Если пользователь забрал оставшиеся 🍬ы
+            bot.send_message(message.chat.id, f'Ты забрал оставшиеся 🍬ы и выиграл 🥳')
             storage.init(message.chat.id) ### Очищает значения из хранилище
             return
-        else: # Если конфеты ещё остались на столе
+        else: # Если 🍬ы ещё остались на столе
+            reply_text = []
             offset = int(user_digit)
             count_sweets -= offset
-            bot.send_message(message.chat.id, f'На столе осталось {count_sweets} конфет')
+            reply_text.append(f'На столе осталось {count_sweets} 🍬')
 
             offset = random.randint(MIN_SWEETS, count_sweets)
             count_sweets -= offset
             storage.set(message.chat.id, "count_sweets", count_sweets)
-            bot.send_message(message.chat.id, f'"Компьютер с руками" забрал со стола {offset} конфет')
+            reply_text.append(f'"Компьютер с руками" забрал со стола {offset} 🍬')
             
-            if count_sweets != 0:
+            if count_sweets != 0: # Если 🍬ы ещё есть на столе
                 bot.register_next_step_handler(message, process_digit_step)
-                bot.send_message(message.chat.id, f'На столе ещё осталось {count_sweets} конфет, у тебя ещё есть все шансы выигарть!')
-            else:
-                bot.send_message(message.chat.id, f'Компьютер выиграл')
+                reply_text.append(f'На столе ещё осталось {count_sweets} 🍬, у тебя ещё есть все шансы выигарть!')
+                reply_text.append(f'Сколько 🍬 вы возьмете? (не более {count_sweets})')
+                bot.send_message(message.chat.id, '\n'.join(reply_text))
+            else: # Если последние 🍬ы забрал компьютер
+                reply_text.append(f'Компьютер выиграл 🥲')
                 storage.init(message.chat.id) ### Очищает значения из хранилище
+                bot.send_message(message.chat.id, '\n'.join(reply_text))
                 return
 
-    @bot.message_handler(func=lambda message: message.text.lower() == "игра")
+    @bot.message_handler(func=lambda message: message.text.lower() == "start")
     def start_game(message):
         storage.init(message.chat.id) ### Инициализирую хранилище
 
@@ -48,11 +55,11 @@ def init(bot: TeleBot):
 
         bot.send_message(message.chat.id, f"""
         Игра "Конфеты"!
-        На столе лежит {count_sweets} конфет
+        На столе лежит {count_sweets} 🍬
         """)
 
         bot.send_message(message.chat.id, f"""
-        Сколько конфет вы возьмете? (не более {count_sweets})
+        Сколько 🍬 вы возьмете? (не более {count_sweets})
         """)
 
         bot.register_next_step_handler(message, process_digit_step)
