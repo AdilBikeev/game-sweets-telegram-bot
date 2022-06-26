a6840960373aee2d85742389f6b0d11852f76d35
AdilBikeev <bikeev1999@mail.ru>
2022-06-26 18:13:10 +0300

    Add storage

---
+++ src/private/storage.py
@@ -0,0 +1,10 @@
+storage = dict()
+
+def init_storage(user_id):
+    storage[user_id] = dict(attempt=None, random_digit=None)
+
+def set_data_storage(user_id, key, value):
+    storage[user_id][key] = value
+
+def get_data_storage(user_id):
+    return storage[user_id]
